3f78b322b7c30893aff338cf6c9644b464dc777d
Dustin Ogan <ogannbc@gmail.com>
2017-11-05 20:51:17 +0000

    updates to config

--- app/main.js
+++ app/main.js
@@ -1,18 +1,20 @@
 const cron = require('node-cron');
-const poop = require ('../controllers/example.js')
-
-// async function runMonitorJob () {
-//       console.log('running a task every 5 second');  
-//       await poop.fetchAsync()
-//       console.log('finished poop call ')
-//   }
-  
+const webCommerceTokens = require ('../controllers/webCommerceJob.js')
+const getTickets = require ('../controllers/getTicketsJob.js')
 
 
 cron.schedule('0,5 * * * * * *', async function runMonitorJob () {
-      console.log('running a task every 5 second');  
-      await poop.fetchAsync()
-      console.log('finished poop call ')
+      console.log('statring webCommerce call')
+      await webCommerceTokens.send()
+      console.log('finished webCommerce Tokens call ')
+  }
+  
+  );
+cron.schedule('0,5 * * * * * *', async function runMonitorJob () {
+      console.log('statring ticket call')
+      await getTickets.send()
+      console.log('finished tickets Tokens call ')
   }
 
-);
+
+  );
