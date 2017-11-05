3b471a9034f126d44ba16a9c3de0df8620c38ef5
Dustin Ogan <ogannbc@gmail.com>
2017-11-05 20:44:15 +0000

    initial cleanup.  Add sample tickets call with no variables

---
+++ controllers/getTicketsJob.js
@@ -0,0 +1,46 @@
+const config = require("config");
+const fetch = require("node-fetch");
+
+
+
+
+
+
+// async function
+async function send () {
+  var tickets = createCall()
+  console.log(tickets.url)
+  console.log(tickets.postParams)
+  let ticketsCall = await fetch(tickets.url, tickets.postParams);
+
+  let json = await ticketsCall.json();
+  
+  //let ticketsJson = await json.result
+  console.log(json);
+  return json
+}
+
+
+
+function createCall() {
+  // always initialize all instance properties
+  var path = config.get("ticketsConfig.path")
+  var server = config.get('SystemConfig.baseUrl')
+  this.url =  server+path
+  console.log(this.url);
+  var postBody = '{"externalGuestId":"5572cef0-9b17-11e7-aaf3-bd4c20405e42","sessionId":"5572cef0-9b17-11e7-aaf3-bd4c20405e42","cards":"Tickets_SC_Web","catalogId":"10551","ip":"ip_Tickets_01","ic":"UO website","firstTimeVisitor":"N","geoLocation":"OUS","ticketsPageNumberOfDays":"3","ticketsPageNumberOfAdults":"2","ticketsPageNumberOfChildren":"0","ticketsPageFloridaResidentFlag":"N","persona":"","attractionExperience":"","attractionInterest":"","attractionLocation":"","mapLocation":"","pageBrowsed":"","ticketsPageSelectedTicketDate":"","dates":"","seasons":"","adobeTags":{"OrlandoFL":"success","TicketsContentNoPurch":"Success","ticketscontentviewnopurchasefl30days":"success","ticketscontentviewnopurchasefl7days":"success"}}'
+  this.postParams = { method: 'POST', body: postBody , headers: { 'Content-Type': 'application/json' }};
+  return this;
+}
+
+
+async function send(){
+    let  tickets = await  createCall()
+    let  ticketsResp = await  fetch(tickets.url, tickets.postParams)
+    let ticketsJson = await ticketsResp.json()
+    console.log(ticketsJson)
+}
+
+module.exports = {
+  send : send
+}
---
+++ controllers/webCommerceJob.js
@@ -0,0 +1,33 @@
+const config = require("config");
+const fetch = require("node-fetch");
+var unregstieredGuest = require('../models/UnregisteredGuestCall.js');
+
+
+var authnPath = config.get("authConfig.path")
+var server = config.get('SystemConfig.baseUrl')
+
+
+// async function
+async function send () {
+  var myGuest = unregstieredGuest.createCall()
+  console.log(myGuest.url)
+  console.log(myGuest.postParams)
+  let guestCall = await fetch(myGuest.url, myGuest.postParams);
+
+  let json = await guestCall.json();
+  
+  let guestId = await json.result.guestProfile.guestId;
+
+  let postParams = { method: 'POST', body: '{"externalGuestId": "${guestId}" }'  , headers: { "Content-Type": "application/json" } };
+  
+  let userTokensCall = await fetch(server + authnPath, postParams)
+
+  let tokens = await userTokensCall.json()
+  console.log(tokens);
+  return tokens;
+}
+
+
+module.exports = {
+  send : send
+}
